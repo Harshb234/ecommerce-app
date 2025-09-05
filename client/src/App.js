6b9da9b0010995474c46830a8c05acef01868b48
Harsh Bambatkar <harshbambatkar0502@gmail.com>
2025-09-05 16:05:08 +0530

    Initial project structure setup of client & server

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+ function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Tailwind is Working! 🎉</h1>
+    </div>
+  );
+}
+
+
+export default App;
